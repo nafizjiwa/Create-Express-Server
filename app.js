028d28d22a65c362fee436e52c23fb461033f8a7
nafizjiwa <56348190+nafizjiwa@users.noreply.github.com>
2022-06-14 16:30:26 -0700

    Update app.js
    Add steps to setting up a server using node.js module EXPRESS

--- app.js
+++ app.js
@@ -1,75 +1,31 @@
-//Start a server using Express (a node module).
-//FIRST, Import the Express library with require.
-//SECOND, invoke EXPRESS which returns an instance of an Express application.
-//This application can then be used to start a server and specify server behavior.
+//START A SERVER USING EXPRESS (a node module).
+FIRST(1), Import the Express library with require.
+SECOND(2), invoke EXPRESS which returns an instance of an Express application.
+  //This application (and its methods) can then be used to start a server and specify server behavior.
+  //The purpose of a server is to listen for requests, perform actions to satisfy the request, and then return a response.
+THIRD(3), In order for our server to start responding, we have to tell the server where to listen for new requests.
+  //Provide a port number argument to a method called app.listen().
+  //Invoking the app.listen() call will start a server listening on chosen port and respond to any requests that comes into it;
+  //Here: PORT - 4001;  Response - 'Server is listening on port 4001' and confirms server started successfully.
+FOURTH(4), To start server listening for requests, run the command > node [app_file_name.js] to run your server in Node.
+FIFTH(5),  To tell our server how to deal with any given request, we register a series of routes. Request’s are based on path's and HTTP verb;
+  //Path is part of request URL http://localhost:4001/routename (http_verb//hostname:port/path)
+  //EXPRESS uses ROUTE HANDLERS app.http_verb(PATH, CALLBACK) to register routes. I TAKES 2 ARGUMENT USUALLY;
+SIXTH(6), If no routes are matched on a client request, the Express server will handle sending a 404 Not Found response to the client.
+SEVENTH(7), GET requests are requesting information from the server
 
-//The purpose of a server is to listen for requests, perform whatever action is required to satisfy the request,
-//  and then return a response. In order for our server to start responding, we have to tell the server where to listen
-// for new requests by providing a port number argument to a method called app.listen(). 
-// The server will then listen on the specified port and respond to any requests that come into it.
-  
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require('express');               (1)
+const app = express();                            (2)
 
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './db.sqlite');
+const PORT = process.env.PORT || 4001;            (3) //where to listen to
 
-const PORT = process.env.PORT || 4001;
+app.use(express.static('public')); // Use static server to serve the Express Yourself Website
 
-app.use(express.static('public'));
-app.use(morgan('dev'));
-app.use(bodyParser.json());
+app.get('/PATH OR ROUTE', (req, res, next) => {CALLBACK Handles request and sends response....});
+    (5) //Server recieves a GET request mathing at /ROUTE or an opened call to app.get();
+app.post('/PATH OR ROUTE', (req, res, next) => {CALLBACK Handles request and sends response...});
+    (5) //Server recieves a POST request mathing at /ROUTE;
 
-app.get('/strips', (req, res, next) => {
-  db.all('SELECT * FROM Strip', (err, rows) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      res.send({strips: rows});
-    }
-  });
-});
-
-const validateStrip = (req, res, next) => {
-  const stripToCreate = req.body.strip;
-  if (!stripToCreate.head || !stripToCreate.body || !stripToCreate.bubbleType ||
-      !stripToCreate.background) {
-    return res.sendStatus(400);
-  }
-  next();
-}
-
-app.post('/strips', validateStrip, (req, res, next) => {
-  const stripToCreate = req.body.strip;
-  db.run(`INSERT INTO Strip (head, body, bubble_type, background, bubble_text,
-    caption) VALUES ($head, $body, $bubbleType, $background, $bubbleText,
-    $caption)`,
-  {
-    $head: stripToCreate.head,
-    $body: stripToCreate.body,
-    $bubbleType: stripToCreate.bubbleType,
-    $background: stripToCreate.background,
-    $bubbleText: stripToCreate.bubbleText,
-    $caption: stripToCreate.caption,
-  }, function(err) {
-    if (err) {
-      return res.sendStatus(500);
-    }
-    db.get(`SELECT * FROM Strip WHERE id = ${this.lastID}`, (err, row) => {
-      if (!row) {
-        return res.sendStatus(500);
-      }
-      res.status(201).send({strip: row});
-    });
-  });
-});
-
-app.listen(PORT, () => {
+app.listen(PORT, () => {                            (3)
   console.log(`Server is listening on port ${PORT}`);
 });
-
-module.exports = app;
-//this was sent on git hub as a
-//test to uploading files onto github
